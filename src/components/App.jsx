519dcec4a975e224ba82378f698adaac189f1deb
toshiroto <chtopete@gmail.com>
2022-03-25 12:37:27 +0900

    Note and Footer component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
+
 import React from "react";
 import { render } from "react-dom";
-const App = () => {
-  return React.createElement("div", {}, React.createElement("h1", {}, "Notes"));
-};
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
 
-render(React.createElement(App), document.getElementById("root"));
-  
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,3 +1,11 @@
-import React from "react";
+function Note () {
+  return (
+    <div className="note">
+      <h1>This is a Note</h1>
+      <p>This is the content of the Note</p>
+    </div>
 
+  );
+}
 
+export default Note;
