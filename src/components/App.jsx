c43749bb375559b9f575ca7201715599ab637b5a
toshiroto <chtopete@gmail.com>
2022-03-25 10:38:14 +0900

    Eslint and Prettier added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
 const App = () => {
-      return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Notes")
-  );
+  return React.createElement("div", {}, React.createElement("h1", {}, "Notes"));
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
