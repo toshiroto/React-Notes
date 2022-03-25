f936d174acea57490c5cc52ddbfaa78b17def016
toshiroto <chtopete@gmail.com>
2022-03-25 10:13:33 +0900

    App component created

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+      return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Notes")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
