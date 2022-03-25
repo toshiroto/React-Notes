9359c1ffb1667058ed5d374ee735b6b5fcc62661
toshiroto <chtopete@gmail.com>
2022-03-25 16:36:08 +0900

    materials UI style added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -26,22 +31,32 @@ function CreateArea(props) {
   event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
+      {isExpanded ? <input
           name="title"
           onChange={handleChange}
           value={note.title}
-          placeholder="Title" />
+          placeholder="Title" /> : null}
+
         <textarea
           name="content"
+          onClick={expand}
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3">
+          rows={isExpanded ? 3 : 1}>
           </textarea>
-        <button onClick={submitNote}>Add</button>
+          <Zoom in={isExpanded}>
+            <Fab onClick={submitNote}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
       </form>
     </div>
   )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,3 +1,5 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+
 function Note (props) {
   function handleClick () {
     props.onDelete(props.id)
@@ -7,7 +9,10 @@ function Note (props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button
+        onClick={handleClick}>
+        <DeleteIcon />
+        </button>
     </div>
 
   );
